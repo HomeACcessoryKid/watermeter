25d98bdd43550ed80a75586ec15f79e97f9890a1
HomeACcessoryKid <homeaccessorykid@gmail.com>
2022-02-19 15:27:35 +0100

    0.4.4 replace inline mqtt code with dedicated shared code base
    common with other accessories
    changed stored counter from wm_zero to wm0 which is 4 bytes shorter

--- Makefile
+++ Makefile
@@ -18,3 +18,8 @@ include $(SDK_PATH)/common.mk
 
 monitor:
 	$(FILTEROUTPUT) --port $(ESPPORT) --baud $(ESPBAUD) --elf $(PROGRAM_OUT)
+
+sig:
+	openssl sha384 -binary -out firmware/main.bin.sig firmware/main.bin
+	printf "%08x" `cat firmware/main.bin | wc -c`| xxd -r -p >>firmware/main.bin.sig
+	ls -l firmware
--- main.c
+++ main.c
@@ -11,10 +11,7 @@
 #include "i2s_dma/i2s_dma.h"
 #include <udplogger.h>
 #include <lwip/apps/sntp.h>
-
-#include <paho_mqtt_c/MQTTESP8266.h>
-#include <paho_mqtt_c/MQTTClient.h>
-#include <semphr.h>
+#include "mqtt-client.h"
 #include <sysparam.h>
 
 #ifndef VERSION
@@ -35,19 +32,13 @@ uint32_t dma_buf[BUFSIZE];
 static   dma_descriptor_t dma_block;
 time_t ts;
 
-SemaphoreHandle_t wifi_alive;
-QueueHandle_t publish_queue;
-#define PUB_MSG_LEN 48  // suitable for a Domoticz counter update
-#define MQTT_PORT  1883
-#define MQTT_topic "domoticz/in"
-char    *mqtthost=NULL, *mqttuser=NULL, *mqttpass=NULL, *dmtczidx=NULL, *counter0=NULL;
+char     *dmtczidx=NULL, *counter0=NULL;
 uint32_t halflitres,halflitres0;
 
 void spike_task(void *argv) {
     int i=0;
-    bool direction=0;
+    bool direction=0; //TODO: determine the correct initial state
     uint16_t reading,min1,min2,min1x,min2x,min1xx,min2xx,min1xxx,min2xxx; // x is for eXtreme which we will ignore
-    char msg[PUB_MSG_LEN];
     halflitres=halflitres0;
     
     //SPIKE_PIN is GPIO3 = RX0 because hardcoded in i2s - remove UART cable from RX0 port!
@@ -106,8 +97,8 @@ void spike_task(void *argv) {
             }
         }
         if (!i) { // i will be WINDOW if no update
-            snprintf(msg, PUB_MSG_LEN, "{\"idx\":%s,\"nvalue\":0,\"svalue\":\"%.1f\"}", dmtczidx, halflitres/2.0);
-            if (xQueueSend(publish_queue, (void *)msg, 0) == pdFALSE) printf("Publish queue overflow.\n");
+            i=mqtt_client_publish("{\"idx\":%s,\"nvalue\":0,\"svalue\":\"%.1f\"}", dmtczidx, halflitres/2.0);
+            if (i<0) printf("MQTT publish of counter failed because %s\n",MQTT_CLIENT_ERROR(i));
         }
         printf("%d %d %d %d %d %d %3.1f %d\n",direction,sdk_system_get_time()/1000,min1xx,min2xx,min1,min2,halflitres/2.0,min1-min2-OFFSET);
     }
@@ -128,10 +119,9 @@ void sntp_task(void *argv) {
     printf("TIME SET: %u=%s", (unsigned int) ts, ctime(&ts));
     uint32_t old_halflitres=0;
     while(1) {
-        printf("user_params: %s %s %s %s %s %d\n",mqtthost,mqttuser,mqttpass,dmtczidx,counter0,halflitres0);
         if (old_halflitres==halflitres) {
             char wm_zero[12]; sprintf(wm_zero,"%u",halflitres);
-            sysparam_set_string("wm_zero",wm_zero);
+            sysparam_set_string("wm0",wm_zero); // sysparam does not actually write if value didn't change !
             printf("wrote %s\n",wm_zero);
         } else {
             old_halflitres=halflitres;
@@ -143,171 +133,25 @@ void sntp_task(void *argv) {
     vTaskDelete(NULL);
 }
 
-static const char *  get_my_id(void)
-{
-    // Use MAC address for Station as unique ID
-    static char my_id[13];
-    static bool my_id_done = false;
-    int8_t i;
-    uint8_t x;
-    if (my_id_done)
-        return my_id;
-    if (!sdk_wifi_get_macaddr(STATION_IF, (uint8_t *)my_id))
-        return NULL;
-    for (i = 5; i >= 0; --i)
-    {
-        x = my_id[i] & 0x0F;
-        if (x > 9) x += 7;
-        my_id[i * 2 + 1] = x + '0';
-        x = my_id[i] >> 4;
-        if (x > 9) x += 7;
-        my_id[i * 2] = x + '0';
-    }
-    my_id[12] = '\0';
-    my_id_done = true;
-    return my_id;
-}
-
-#define BACKOFF1 100/portTICK_PERIOD_MS
-static void  mqtt_task(void *pvParameters)
-{
-    int ret         = 0;
-    int backoff = BACKOFF1;
-    struct mqtt_network network;
-    mqtt_client_t client   = mqtt_client_default;
-    char mqtt_client_id[20];
-    uint8_t mqtt_buf[100];
-    uint8_t mqtt_readbuf[100];
-    mqtt_packet_connect_data_t data = mqtt_packet_connect_data_initializer;
-    char msg[PUB_MSG_LEN];
-
-    mqtt_network_new( &network );
-    memset(mqtt_client_id, 0, sizeof(mqtt_client_id));
-    strcpy(mqtt_client_id, "ESP-");
-    strcat(mqtt_client_id, get_my_id());
-
-    data.willFlag       = 0;
-    data.MQTTVersion    = 3;
-    data.clientID.cstring   = mqtt_client_id;
-    data.username.cstring   = mqttuser;
-    data.password.cstring   = mqttpass;
-    data.keepAliveInterval  = 10;
-    data.cleansession   = 0;
-
-    while(1) {
-        xSemaphoreTake(wifi_alive, portMAX_DELAY);
-        printf("%s: started\n", __func__);
-        printf("%s: (Re)connecting to MQTT server %s ... ",__func__,
-               mqtthost);
-        ret = mqtt_network_connect(&network, mqtthost, MQTT_PORT);
-        if( ret ){
-            printf("error: %d\n", ret);
-            vTaskDelay(backoff);
-            if (backoff<BACKOFF1*128) backoff*=2; //max out at 12.8 seconds
-            continue;
-        }
-        printf("done\n");
-        backoff = BACKOFF1;
-        mqtt_client_new(&client, &network, 5000, mqtt_buf, 100,
-                      mqtt_readbuf, 100);
-
-        printf("Send MQTT connect ... ");
-        ret = mqtt_connect(&client, &data);
-        if(ret){
-            printf("error: %d\n", ret);
-            mqtt_network_disconnect(&network);
-            vTaskDelay(backoff);
-            if (backoff<BACKOFF1*128) backoff*=2; //max out at 12.8 seconds
-            continue;
-        }
-        printf("done\n");
-        backoff = BACKOFF1;
-        xQueueReset(publish_queue);
-
-        while(1){
-
-            msg[PUB_MSG_LEN - 1] = 0;
-            while(xQueueReceive(publish_queue, (void *)msg, 0) == pdTRUE){
-                printf("got message to publish\n");
-                mqtt_message_t message;
-                message.payload = msg;
-                message.payloadlen = strlen(msg);
-                message.dup = 0;
-                message.qos = MQTT_QOS1;
-                message.retained = 0;
-                ret = mqtt_publish(&client, MQTT_topic , &message);
-                if (ret != MQTT_SUCCESS ){
-                    printf("error while publishing message: %d\n", ret );
-                    break;
-                }
-            }
-
-            ret = mqtt_yield(&client, 1000);
-            if (ret == MQTT_DISCONNECTED)
-                break;
-        }
-        printf("Connection dropped, request restart\n");
-        mqtt_network_disconnect(&network);
-        taskYIELD();
-    }
-}
-
-static void  wifi_task(void *pvParameters)
-{
-    uint8_t status  = 0;
-    uint8_t retries = 30;
-    while(1)
-    {
-        while ((status != STATION_GOT_IP) && (retries)){
-            status = sdk_wifi_station_get_connect_status();
-            printf("%s: status = %d\n", __func__, status );
-            if( status == STATION_WRONG_PASSWORD ){
-                printf("WiFi: wrong password\n");
-                break;
-            } else if( status == STATION_NO_AP_FOUND ) {
-                printf("WiFi: AP not found\n");
-                break;
-            } else if( status == STATION_CONNECT_FAIL ) {
-                printf("WiFi: connection failed\n");
-                break;
-            }
-            vTaskDelay( 1000 / portTICK_PERIOD_MS );
-            --retries;
-        }
-        if (status == STATION_GOT_IP) {
-            printf("WiFi: Connected\n");
-            xSemaphoreGive( wifi_alive );
-            taskYIELD();
-        }
-
-        while ((status = sdk_wifi_station_get_connect_status()) == STATION_GOT_IP) {
-            xSemaphoreGive( wifi_alive );
-            taskYIELD();
-        }
-        printf("WiFi: disconnected\n");
-        sdk_wifi_station_disconnect();
-        vTaskDelay( 1000 / portTICK_PERIOD_MS );
-    }
-}
-
+mqtt_config_t mqttconf=MQTT_DEFAULT_CONFIG;
 char error[]="error";
 void ota_string() {
-    //sysparam_set_string("ota_string", "192.168.178.5;WaterMeter;testingonly;62;41532"); //can be used if not using LCM
     char *otas,*wm0;
     if (sysparam_get_string("ota_string", &otas) == SYSPARAM_OK) {
-        mqtthost=strtok(otas,";");
-        mqttuser=strtok(NULL,";");
-        mqttpass=strtok(NULL,";");
-        dmtczidx=strtok(NULL,";");
-        counter0=strtok(NULL,";");
+        mqttconf.host=strtok(otas,";");
+        mqttconf.user=strtok(NULL,";");
+        mqttconf.pass=strtok(NULL,";");
+        dmtczidx     =strtok(NULL,";");
+        counter0     =strtok(NULL,";");
     }
-    if (mqtthost==NULL) mqtthost=error;
-    if (mqttuser==NULL) mqttuser=error;
-    if (mqttpass==NULL) mqttpass=error;
-    if (dmtczidx==NULL) dmtczidx=error;
-    if (counter0==NULL) counter0=error;
+    if (mqttconf.host==NULL) mqttconf.host=error;
+    if (mqttconf.user==NULL) mqttconf.user=error;
+    if (mqttconf.pass==NULL) mqttconf.pass=error;
+    if (dmtczidx     ==NULL) dmtczidx     =error;
+    if (counter0     ==NULL) counter0     =error;
     halflitres0=2*atof(counter0);
-    if (sysparam_get_string("wm_zero", &wm0) == SYSPARAM_OK) halflitres0=atoi(wm0);
+    if (sysparam_get_string("wm0", &wm0) == SYSPARAM_OK) halflitres0=atoi(wm0);
+    else if (sysparam_get_string("wm_zero", &wm0) == SYSPARAM_OK) halflitres0=atoi(wm0); //transition only
     //and RTC value if available
 }
 
@@ -315,13 +159,11 @@ void user_init(void) {
     uart_set_baud(0, 115200);
     udplog_init(2);
     UDPLUS("\n\n\nWaterMeter " VERSION "\n");
+    //sysparam_set_string("ota_string", "192.168.178.5;WaterMeter;testingonly;62;41532"); //can be used if not using LCM
     ota_string();
+    mqtt_client_init(&mqttconf);
     gpio_enable( COIL1_PIN, GPIO_OUTPUT); gpio_write( COIL1_PIN, 1);
     gpio_enable( COIL2_PIN, GPIO_OUTPUT); gpio_write( COIL2_PIN, 1);
     xTaskCreate(spike_task, "Spike", 512, NULL, 3, NULL);
     xTaskCreate( sntp_task, "SNTP" , 512, NULL, 1, NULL);
-    vSemaphoreCreateBinary(wifi_alive);
-    publish_queue = xQueueCreate(3, PUB_MSG_LEN);
-    xTaskCreate(&wifi_task, "wifi_task",  256, NULL, 1, NULL);
-    xTaskCreate(&mqtt_task, "mqtt_task", 1024, NULL, 2, NULL);
 }
